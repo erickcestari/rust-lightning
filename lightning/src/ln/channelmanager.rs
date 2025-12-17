74f22528edcc090aa39050f15809d8454d3c5e87
Erick Cestari <erickcestari03@gmail.com>
2025-12-17 15:14:23 -0300

    expose some internals

--- lightning/src/ln/channelmanager.rs
+++ lightning/src/ln/channelmanager.rs
@@ -7440,7 +7440,7 @@ where
 								&onion_packet.public_key.unwrap(),
 								&onion_packet.hop_data,
 								onion_packet.hmac,
-								payment_hash,
+								Some(payment_hash),
 								None,
 								&*self.node_signer,
 							);
